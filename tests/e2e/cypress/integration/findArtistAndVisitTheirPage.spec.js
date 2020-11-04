4b5d5b2d7327f0429231c03137ee37ca737b3357
Craig Forrest <craigforrest@Craigs-MacBook-Pro.local>
2020-11-04 13:37:48 +0000

    fix: remove localhost from e2e assert

--- tests/e2e/cypress/integration/findArtistAndVisitTheirPage.spec.js
+++ tests/e2e/cypress/integration/findArtistAndVisitTheirPage.spec.js
@@ -8,6 +8,6 @@ context("sample", () => {
   it("should have title text", () => {
     cy.get("input").type("Kanye west");
     cy.get("a").contains("Kanye West").click();
-    cy.url().should("eq", "http://localhost:3000/artists/5K4W6rqBFWDnAN6FQUkS6x/chart");
+    cy.url().should("include", "/artists/5K4W6rqBFWDnAN6FQUkS6x/chart");
   });
 });
