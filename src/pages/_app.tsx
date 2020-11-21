f2f44c90781cc9f226eddcc6f9c1f528f15d3662
Craig Forrest <craforrest97@gmail.com>
2020-11-21 12:49:19 +0000

    Improve styling (#7)
    * add padding to card__inner
    
    * ignore :any eslint rule
    
    Co-authored-by: Craig Forrest <craigforrest@Craigs-MacBook-Pro.local>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import { AppProps } from "next/dist/next-server/lib/router/router";
 import "../styles/import.scss";
 
-const App = ({ Component, pageProps }: any) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps): ReactNode => <Component {...pageProps} />;
 
 export default App;
