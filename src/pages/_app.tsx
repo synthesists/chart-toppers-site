0f22861c5a82065f4773979650d80ba2bccfbc40
Craig Forrest <craforrest97@gmail.com>
2020-12-19 19:22:51 +0000

    Add font refactor styles (#12)
    * add Poppins font
    
    * add keyboard focus improvements

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import "../styles/import.scss";
+import "focus-visible/dist/focus-visible";
 
 const App = ({ Component, pageProps }: AppProps): ReactNode => <Component {...pageProps} />;
 
